b15e595989b673b358f5d08678d9918640c257cb
Techno Codist <technocodist@gmail.com>
2025-06-27 10:31:23 +0300

    checked new approach to work as before

--- AITCSM.NET/Program.cs
+++ AITCSM.NET/Program.cs
@@ -2,3 +2,13 @@
 using AITCSM.NET.Implementations.Simulation.CH01;
 
 await DistributionOfMoney.DefaultSimulate();
+await DistributionOfMoney.DefaultPlot();
+
+await DistributionOfMoneyWithSaving.DefaultSimulate();
+await DistributionOfMoneyWithSaving.DefaultPlot();
+
+await FreeFall.DefaultSimulate();
+await FreeFall.DefaultPlot();
+
+await FreeFallWithAirResistance.DefaultSimulate();
+await FreeFallWithAirResistance.DefaultPlot();
